f8b5ec404497863fac73ab6a65ca90551e854d90
ti93 <414162330@qq.com>
2020-03-26 21:55:32 +0800

    1

--- springboot-service/src/main/java/com/springboot/Application.java
+++ springboot-service/src/main/java/com/springboot/Application.java
@@ -14,9 +14,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
  *
  *https://blog.csdn.net/valada/article/details/80892573
  */
-@SpringBootApplication(scanBasePackages = "com.springboot")
-@MapperScan(basePackages = {"com.springboot.dao"})
 @EnableAsync
+@MapperScan(basePackages = {"com.springboot.dao"})
+@SpringBootApplication(scanBasePackages = "com.springboot")
 public class Application {
 
 

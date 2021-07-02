8a482a96449c06276fcd9448fe7a01e01eeb7b8f
田江南 <tianjn@hrtps.com>
2021-07-02 11:52:00 +0800

    dev

--- springboot-service/src/main/java/com/springboot/controller/ExcelController.java
+++ springboot-service/src/main/java/com/springboot/controller/ExcelController.java
@@ -1,38 +1,32 @@
 package com.springboot.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletResponse;
-
-import com.alibaba.excel.EasyExcel;
-
-import com.google.common.collect.Lists;
 import com.springboot.common.entity.Result;
 import com.springboot.common.enums.ResultCode;
 import com.springboot.common.exception.ServiceRuntimeException;
+import com.springboot.common.util.ResultUtil;
 import com.springboot.dao.dto.UserDTO;
 import com.springboot.entity.excel.DownloadData;
 import com.springboot.entity.excel.UploadData;
-import com.springboot.util.BeanCopyUtils;
-import com.springboot.util.ExcelUploadListener;
 import com.springboot.service.repository.UserRepository;
+import com.springboot.util.BeanCopyUtils;
 import com.springboot.util.ExcelUtil;
-import com.springboot.common.util.ResultUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
 @RequestMapping("/excel")
 public class ExcelController {
+
+
     @Autowired
     UserRepository userRepository;
 
@@ -40,48 +34,22 @@ public class ExcelController {
      * @Desc 批量导出
      **/
     @PostMapping(value = "/export/user")
-    public Result exportUser(HttpServletResponse response) {
-        try {
-            List<UserDTO> userDTOS = userRepository.getUserDTOS();
-            List<DownloadData> data = userDTOS.stream().map(x -> BeanCopyUtils.copyProperties(x, DownloadData.class)).collect(Collectors.toList());
-            ExcelUtil.exportExcel(response, DownloadData.class, data, "用户信息");
-            return ResultUtil.success();
-        } catch (Exception e) {
-            log.error("导出用户信息失败", e);
-            throw new ServiceRuntimeException(ResultCode.INVALID_PARAMETER);
-        }
-    }
-
-    /**
-     * @Desc 批量导入
-     **/
-    @PostMapping(value = "/import/user", headers = "content-type=multipart/form-data")
-    public Result importUser(@RequestParam MultipartFile file) {
-        try {
-            List<UploadData> importDatas = ExcelUtil.importExcel(file,
-                UploadData.class);
-            userRepository.addUser(importDatas.stream().map(this::convert).collect(Collectors.toList()));
-            return ResultUtil.success();
-        } catch (Exception e) {
-            log.error("用户导入流关闭失败", e);
-            return ResultUtil.error(ResultCode.UNKNOWN_ERROR, e.getMessage());
-        }
+    public void exportUser(HttpServletResponse response) {
+        List<UserDTO> userDTOS = userRepository.getUserDTOS();
+        List<DownloadData> data = userDTOS.stream().map(x -> BeanCopyUtils.copyProperties(x, DownloadData.class)).collect(Collectors.toList());
+        ExcelUtil.exportExcel(response, DownloadData.class, data, "用户信息");
     }
 
     /**
      * 文件上传
-     * 1. 创建excel对应的实体对象 参照{@link UploadData}
-     * 2. 由于默认异步读取excel，所以需要创建excel一行一行的回调监听器，参照{@link ExcelUploadListener}
-     * 3. 直接读即可
      */
     @PostMapping("/upload/user")
-    public Result upload(MultipartFile file) throws IOException {
-        EasyExcel.read(file.getInputStream(), UploadData.class, new ExcelUploadListener<>(this::convert, t -> userRepository.addUser(t))).sheet()
-            .doRead();
+    public Result upload(MultipartFile file) {
+        ExcelUtil.upload(file, UploadData.class, this::convert, t -> userRepository.addUser(t));
         return ResultUtil.success();
     }
 
-    private UserDTO convert(UploadData uploadData){
-       return BeanCopyUtils.copyProperties(uploadData,UserDTO.class);
+    private UserDTO convert(UploadData uploadData) {
+        return BeanCopyUtils.copyProperties(uploadData, UserDTO.class);
     }
 }
--- springboot-service/src/main/java/com/springboot/util/ExcelUtil.java
+++ springboot-service/src/main/java/com/springboot/util/ExcelUtil.java
@@ -1,46 +1,56 @@
 package com.springboot.util;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletResponse;
-
 import com.alibaba.excel.EasyExcel;
-
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
-@Slf4j
-public class ExcelUtil<T> {
 
-    public static void exportExcel(HttpServletResponse response,
-                                   Class head, List<?> listData, String fileName) throws IOException {
-        if(CollectionUtils.isEmpty(listData)){
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@Slf4j
+public class ExcelUtil {
+
+
+    public static <T> void exportExcel(HttpServletResponse response,
+                                   Class<T> head, List<T> listData, String fileName)  {
+        if (CollectionUtils.isEmpty(listData)) {
             return;
         }
-        log.info("导出 Excel 数据 [{}], {} 条",fileName,listData.size());
-        fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8.name());
-        response.setContentType("application/vnd.ms-excel");
-        response.setCharacterEncoding("UTF-8");
-        response.setHeader("Content-Disposition", "attachment;filename=" + fileName + ".xlsx");
-        EasyExcel.write(response.getOutputStream(), head).sheet("sheet0").doWrite(listData);
+        try {
+            log.info("导出 Excel 数据 [{}], {} 条", fileName, listData.size());
+            fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8.name());
+            response.setContentType("application/vnd.ms-excel;charset=UTF-8");
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+            // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+            response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
+            EasyExcel.write(response.getOutputStream(), head).sheet("sheet0").doWrite(listData);
+        } catch (IOException e) {
+            log.error("Excel导出失败，Class:{}", head.getName(), e);
+        }
     }
 
-    public static <T> List<T> importExcel(MultipartFile file, Class<T> head) {
-        List<T> data = new ArrayList<>();
-        try (InputStream inputStream = new BufferedInputStream(file.getInputStream())) {
-            ExcelListener<T> excelListener = new ExcelListener<>(data);
-            //执行完，才执行listener
-            EasyExcel.read(inputStream, head, excelListener).sheet().doRead();
-            return data;
-        } catch (Exception e) {
-            e.printStackTrace();
+    /**
+     *
+     * @param file 上传文件
+     * @param clazz 解析的类
+     * @param converter 解析的类-->数据库DTO
+     * @param consumer 插入数据
+     * @param <T> 解析的类
+     * @param <R> 数据库DTO
+     */
+    public static <T, R> void upload(MultipartFile file, Class<T> clazz, Function<T, R> converter, Consumer<List<R>> consumer) {
+        try {
+            EasyExcel.read(file.getInputStream(), clazz, new ExcelListener<>(converter, consumer)).sheet()
+                    .doRead();
+        } catch (IOException e) {
+            log.error("Excel导入失败，Class:{}", clazz.getName(), e);
         }
-        return data;
     }
+
 }
